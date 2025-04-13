bc682b45a57638d0cf1c6e85176f1b3f03463760
SColson82 <sw-slcolson@login.localdomain>
2025-04-12 20:47:19 -0500

    running pipeline on HPC on batch-impulse, 28 pcus-per-task

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=hpcadmins
+#SBATCH --partition=batch-impulse  
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=28
 #SBATCH --time=4:00:00
 #SBATCH --array=0-26
 
 # Load your environment
-module load python/3.9
-source ~/envs/nlp/bin/activate
+spack load py-matplotlib@3.8 py-pandas py-seaborn py-scikit-learn 
+# source ../tweet_env/bin/activate
 
 # Read the dataset name from the array index
 DATASET_NAME=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" dataset_list.txt)
